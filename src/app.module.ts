0a9d273ce3aacd4464d0efd1594adf99d9674c87
Zamba-Vitor <zambaestuda@gmail.com>
2026-05-09 15:31:32 -0300

    Criação da conexão com o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './users/user.module';
+import { RotinaModule } from './rotina/rotina.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+    }),
+    
+    // 2. Coloque seus módulos novos aqui para o NestJS ativar as rotas deles!
+    UserModule,
+    RotinaModule,
+  ],
+  
+  // 3. Deixamos as listas abaixo vazias, pois apagamos o AppController e o AppService
+  controllers: [], 
+  providers: [],   
+  
 })
 export class AppModule {}
