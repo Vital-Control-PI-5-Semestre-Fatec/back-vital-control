4604c0768fe5863dcf32228d969f7a28f329f69f
Zamba-Vitor <zambaestuda@gmail.com>
2026-05-09 15:12:10 -0300

    conexão do banco com o back-end

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // <-- Adicionamos o ConfigService aqui
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'), 
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { describe, it, beforeEach, afterEach } from '@jest/globals';
+
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
